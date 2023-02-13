e902d45324d28a372af0ec8b981cbcd91b8afb10
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2023-02-13 03:31:12 +0000

    session 1

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,11 +1,10 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let x;
-    if x == 10 {
+    let x: f32 = 0.0;
+    if x == 10.0 {
         println!("x is ten!");
     } else {
         println!("x is not ten!");
